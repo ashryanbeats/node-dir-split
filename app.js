56578441fe9f3437f5bbe0734e87b0aa0dda81c4
Ash Ryan <ash@ashryanbeats.com>
2015-04-06 20:41:15 -0400

    Now reading directory and file stats for write path.

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 var fs = require('fs');
 
 var readDirPath = process.argv[2];
+var writeDirPath = process.argv[3];
+var pathToRead;
 
 fs.readdir(readDirPath, function(err, files){
 	if (err) return done(err);
-	
+
+	pathToRead = readDirPath;
+
+	console.log("READING PATH: ", pathToRead);
+
 	files.forEach(getFileInfo);
+
+	fs.readdir(writeDirPath, function(err, files) {
+		if (err) return done(err);
+
+		pathToRead = writeDirPath;
+
+		console.log("READING PATH: ", pathToRead);
+
+		files.forEach(getFileInfo);
+	});
 });
+
 
 
 function done(err) {
@@ -14,7 +31,7 @@ function done(err) {
 }
 
 function getFileInfo(filename) {
-	fs.stat(readDirPath + '/' + filename, function(err, stats) {
+	fs.stat(pathToRead + '/' + filename, function(err, stats) {
 		if (err) return done(err);
 		console.log(filename);
 		console.log(stats);
