92b6ef3be2f4d2ecbdcf51ea6474c4bde9ceaa93
Ash Ryan <ash@ashryanbeats.com>
2015-04-06 21:11:20 -0400

    Having trouble after promisifying fs.

--- app.js
+++ app.js
@@ -1,28 +1,34 @@
-var fs = require('fs');
+var Promise = require('bluebird');
+var fs = Promise.promisifyAll(require('fs'));
 
 var readDirPath = process.argv[2];
 var writeDirPath = process.argv[3];
 var pathToRead;
 
-fs.readdir(readDirPath, function(err, files){
-	if (err) return done(err);
-
-	pathToRead = readDirPath;
-
-	console.log("READING PATH: ", pathToRead);
-
-	files.forEach(getFileInfo);
-
-	fs.readdir(writeDirPath, function(err, files) {
-		if (err) return done(err);
-
-		pathToRead = writeDirPath;
-
-		console.log("READING PATH: ", pathToRead);
-
-		files.forEach(getFileInfo);
-	});
+fs.readdir(readDirPath).forEach(getFileInfo).then(function(files) {
+	console.log(files);
 });
+
+
+// fs.readdir(readDirPath, function(err, files){
+// 	if (err) return done(err);
+
+// 	pathToRead = readDirPath;
+
+// 	console.log("READING PATH: ", pathToRead);
+
+// 	files.forEach(getFileInfo);
+
+// 	// fs.readdir(writeDirPath, function(err, files) {
+// 	// 	if (err) return done(err);
+
+// 	// 	pathToRead = writeDirPath;
+
+// 	// 	console.log("READING PATH: ", pathToRead);
+
+// 	// 	files.forEach(getFileInfo);
+// 	// });
+// });
 
 
 
